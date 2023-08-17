b8e1bb085367936201f8545ac298fb3c30b2caa6
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-17 10:28:43 +0900

    Create Ipaylod.ts

---
+++ src/components/announcement/Ipaylod.ts
@@ -0,0 +1,10 @@
+export type Ipayload = {
+  clubName?: string;
+  title?: string;
+  position?: string;
+  employmentType?: string;
+  detailContent?: string;
+  startDate?: string;
+  endDate?: string;
+  isOpen: boolean;
+};
