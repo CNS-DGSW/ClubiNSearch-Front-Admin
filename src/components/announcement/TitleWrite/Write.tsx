90d6a7e3df2a3fc72d9a6ad38afcc852bc6671ee
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-17 09:34:24 +0900

    :recycle: [announce page] Fix MD Preview

--- src/components/announcement/TitleWrite/Write.tsx
+++ src/components/announcement/TitleWrite/Write.tsx
@@ -4,12 +4,26 @@ import ClubName from "../Select/ClubName";
 import EmploymentType from "../Select/EmploymentType";
 import MainWrite from "@/components/announcement/MainWrite/Write";
 import { useRecoilState } from "recoil";
-import { titleAtom, startDateAtom, endDateAtom } from "@/store/WriteAtom";
+import {
+  clubNameAtom,
+  employmentTypeAtom,
+  positionAtom,
+  titleAtom,
+  startDateAtom,
+  endDateAtom,
+  isOpenAtom,
+} from "@/store/WriteAtom";
+import { useEffect } from "react";
 
 const Write = () => {
+  const [clubName, setClubName] = useRecoilState(clubNameAtom);
+  const [employmentType, setEmploymentType] =
+    useRecoilState(employmentTypeAtom);
+  const [position, setPosition] = useRecoilState(positionAtom);
   const [title, setTitle] = useRecoilState(titleAtom);
   const [startDate, setStartDate] = useRecoilState(startDateAtom);
   const [endDate, setEndDate] = useRecoilState(endDateAtom);
+  const [isOpen, setIsOpen] = useRecoilState<boolean>(isOpenAtom);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
@@ -33,18 +47,55 @@ const Write = () => {
             <S.contentPoint>*</S.contentPoint>
           </S.content>
           <ClubName />
+          {isOpen && clubName == "" && (
+            <div
+              style={{
+                color: "red",
+                fontSize: "13px",
+                marginTop: "5px",
+                marginBottom: "-3.4%",
+              }}
+            >
+              *소속 동아리를 선택해주세요.*
+            </div>
+          )}
 
           <S.content>
             <S.contentTitle>채용 직급</S.contentTitle>
             <S.contentPoint>*</S.contentPoint>
           </S.content>
           <EmploymentType />
+          {isOpen && employmentType == "" && (
+            <div
+              style={{
+                color: "red",
+                fontSize: "13px",
+                marginTop: "5px",
+                marginBottom: "-3.4%",
+              }}
+            >
+              *채용 직급을 선택해주세요.*
+            </div>
+          )}
 
           <S.content>
             <S.contentTitle>채용 포지션</S.contentTitle>
             <S.contentPoint>*</S.contentPoint>
           </S.content>
           <Position />
+          {isOpen && position == "" && (
+            <div
+              style={{
+                color: "red",
+                fontSize: "13px",
+                marginTop: "5px",
+                marginBottom: "-3.4%",
+              }}
+            >
+              *포지션을 선택해주세요.*
+            </div>
+          )}
+
           <S.content>
             <S.contentTitle>채용 기간</S.contentTitle>
             <S.contentPoint>*</S.contentPoint>
@@ -64,6 +115,31 @@ const Write = () => {
               onChange={handleEndDateChange}
             ></S.dateSelect>
           </S.selectContainer>
+          {isOpen && startDate == "" && (
+            <div
+              style={{
+                color: "red",
+                fontSize: "13px",
+                marginTop: "5px",
+                marginBottom: "-3.4%",
+              }}
+            >
+              *채용시작 기간을 선택해주세요.*
+            </div>
+          )}
+          {isOpen && endDate == "" && (
+            <div
+              style={{
+                color: "red",
+                fontSize: "13px",
+                marginLeft: "50%",
+                marginTop: "5px",
+                marginBottom: "-3.4%",
+              }}
+            >
+              *채용종료 기간을 선택해주세요.*
+            </div>
+          )}
         </S.titleContent>
       </S.titleContainer>
       <MainWrite />
